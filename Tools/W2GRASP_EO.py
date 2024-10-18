4b5a59e69c0ed77dca3ea4c7916d847c1eefcee4
ren <xiaodongrenpmo@gmail.com>
2024-10-18 14:12:23 +0000

    update GRASP objectors

--- Tools/W2GRASP_EO.py
+++ Tools/W2GRASP_EO.py
@@ -62,7 +62,11 @@ def write_single_po(name,freq,
                     scatterer,
                     method=0,
                     po_points = [0,0],
-                    ptd_points= [-1,0]):
+                    ptd_points= [[-1,0]],
+                    factor= [0,0],
+                    spill_over='off',
+                    coord_sys=None,
+                    outputfile = ''):
     Method=['po_plus_ptd',
             'po',
             'ptd']
@@ -73,17 +77,33 @@ def write_single_po(name,freq,
     Str+='  method         :'+Method[method]+',\n'
     Str+='  po_points      :'+'struct(po1:'+str(po_points[0])+', po2:' + str(po_points[1])+'),\n'
     Str+='  ptd_points     :'+'sequence(\n'
-    Str+='                              struct(edge:'+ '1'
-
+    for item in ptd_points:
+        Str+='                              struct(edge:'+ str(item[0])+',\n'
+        Str+='                                     ptd:'+ str(item[1])+'),\n'
+    Str+='                              ),\n'
+    Str+='  factor         :struct(db:'+str(factor[0])+', deg:'+str(factor[1])+'),\n'
+    Str+='  spill_over     :'+spill_over+',\n'
+    if coord_sys !=None:
+        Str+='  coor_sys       :ref('+coord_sys+'),\n'
+    if outputfile =='':
+        Str+='  file_name  :'+name+'.po.cur\n)\n\n'
+    else:
+        Str+='  file_name      : '+outputfile+'\n)\n\n'
     return Str
 
 '''4.  write multi po simulations'''    
-def write_multi_po(name,freq,scatterer,outputfile='',method='po_plus_ptd'):
+def write_multi_po(name,freq,
+                   scatterer,
+                   outputfile='',
+                   method=0):
+    Method=['po_plus_ptd',
+            'po',
+            'ptd']
     Str=''
     Str+=name+'.po  '+'po_multi_face_scatterer\n(\n'
     Str+='  frequency      : ref('+freq+'),\n'
     Str+='  scatterer      : ref('+scatterer+'),\n'
-    Str+='  method         : '+method+',\n'
+    Str+='  method         : '+Method[method]+',\n'
     Str+='  ptd_contributions : all,\n'
     if outputfile=='':
         Str+='  file_name      : '+name+'.po.cur\n)\n\n'
@@ -91,7 +111,64 @@ def write_multi_po(name,freq,scatterer,outputfile='',method='po_plus_ptd'):
         Str+='  file_name      : '+outputfile+'\n)\n\n'
     return Str
     
+'''5. write PO lens'''
+
+def write_lens_po(name, freq, 
+                  lens, 
+                  get_field='lens_in_screen',
+                  method='go_plus_po', waist_radius=0,
+                  po_points={'face1':[0,0],'face2': [0,0]}, factor=[0,0],
+                  spill_over='off',coord_sys='', 
+                  current_file_face1='', 
+                  current_file_face2='',
+                  gbc_file=''):
+    '''
+    get field: lens_in_screen, lens_in_free_space, face1, face2
     
+    '''
+    Str=''
+    Str+= name + '.po  '+ 'po_lens\n(\n'
+    Str+= '  frequence     :ref('+freq+'),\n'
+    Str+= '  lens          :ref('+lens+'),\n'
+    Str+= '  get_field     :'+get_field+',\n'
+    Str+= '  method        :'+method+',\n'
+    if method == 'gauss_laguerre':
+        Str+= '  method        :'+method+',\n'
+        if waist_radius==0:
+            Str+= '  waist_radius  :0,\n'
+        else:
+            Str+= '  waist_radius  :squence(\n'
+            for item in waist_radius:
+                Str+= '  waist_radius  :squence(\n'
+                Str+= '                        '+str(item)+',\n'
+            Str+='                          ),\n'
+    Str+= '  po_points     :struct(face1_po1:'+str(po_points['face1'][0])
+    Str+= ', face1_po2:'+str(po_points['face1'][1])+',\n'
+    Str+= '                           face2_po1:'+str(po_points['face2'][0])
+    Str+= ', face2_po2:'+str(po_points['face2'][1])+'),\n'
+    Str+= '  factor        :struct(db:'+str(factor[0])+',deg:'+str(factor[1])+'),\n'
+    Str+= 'spill_over      :'+spill_over+',\n'
+    if coord_sys !='':
+        Str+= 'coor_sys        :ref('+coord_sys+'),\n'
+    if current_file_face1 =='':
+        Str+= 'current_file_face1 :'+name+'face1.po.cur,\n'
+    else:
+        Str+= 'current_file_face1 :'+current_file_face1+'.po.cur,\n'
+    if current_file_face2 =='':
+        Str+= 'current_file_face2 :'+name+'face2.po.cur,\n'                        
+    else:
+        Str+= 'current_file_face2 :'+current_file_face2+'.po.cur,\n'
+
+    Str+=')\n\n'
+
+''' write Aperture in screen PO'''
+
+#MoM
+
+''' write MoM'''
+def write_MoM(name,):
+    pass
+
 '''8. write spherical grid field'''
 def write_spherical_grid(name,coor_sys,u_range,v_range,u0,v0,Nu,Nv,near_far='near',near_dist=100,filename=''):
     Str=''
@@ -124,7 +201,3 @@ def write_planar_grid(name,coor_sys,near_dist=0,x_range=[-1,1,11],y_range=[-1,1,
     Str+=')\n\n'
     return Str
     
-
-
-
-
